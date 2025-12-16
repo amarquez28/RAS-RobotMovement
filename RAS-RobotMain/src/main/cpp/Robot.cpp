fbe49e0cc9d0ed28db682fdeefeebbf53608b7b2
amarquez28 <zhukow28@gmail.com>
2025-12-16 13:36:54 -0700

    Checkpoint 12-16-25

--- RAS-RobotMain/src/main/cpp/Robot.cpp
+++ RAS-RobotMain/src/main/cpp/Robot.cpp
@@ -27,6 +27,17 @@ Robot::Robot() : frc::TimesliceRobot{5_ms, 10_ms} {
   // m_chooser.SetDefaultOption(kAutoNameDefault, kAutoNameDefault);
   // m_chooser.AddOption(kAutoNameCustom, kAutoNameCustom);
   frc::SmartDashboard::PutData("Auto Modes", &m_chooser);
+
+  //distance per pulse
+  //must be calculted for specific robot
+  //formula (wheel circumfrence (M))/ (Encoder pulses per revolution)
+  // Example: 6 inch wheel (0.1524m dia) -> Circumference = 0.478m
+  // If encoder has 360 ticks per rev: 0.478 / 360 = 0.00132
+  m_encoder.SetDistancePerPulse(0.00132); // Replace with our calculated value
+
+  //PID Tolerance
+  //we are done if we are within 0.05 meters of target
+  m_pid.SetTolerance(0.05)
 }
 
 /**
@@ -37,34 +48,25 @@ Robot::Robot() : frc::TimesliceRobot{5_ms, 10_ms} {
  * <p> This runs after the mode specific periodic functions, but before
  * LiveWindow and SmartDashboard integrated updating.
  */
-void Robot::RobotPeriodic() {}
-
-/**
- * This autonomous (along with the chooser code above) shows how to select
- * between different autonomous modes using the dashboard. The sendable chooser
- * code works with the Java SmartDashboard. If you prefer the LabVIEW Dashboard,
- * remove all of the chooser code and uncomment the GetString line to get the
- * auto name from the text box below the Gyro.
- *
- * You can add additional auto modes by adding additional comparisons to the
- * if-else structure below with additional strings. If using the SendableChooser
- * make sure to add them to the chooser code above as well.
- */
-
-void Robot::moveFwd(double speed){
-  if(speed > 1.0){
-    speed = 1.0;
-  }
-
-  if(speed < 0.0){
-    speed = 0.0;
-  }
-
-  m_in1.Set(true);
-  m_in2.Set(false);
-  m_pwm.SetSpeed(speed);
-
+void Robot::RobotPeriodic() {
+  frc::SmartDashboard::PutNumber("Encoder Dist", m_encoder.GetDistance());
+  frc::SmartDashboard::PutNumber("Encoder Rate", m_encoder.GetRate());
 }
+
+// void Robot::moveFwd(double speed){
+//   if(speed > 1.0){
+//     speed = 1.0;
+//   }
+
+//   if(speed < 0.0){
+//     speed = 0.0;
+//   }
+
+//   m_in1.Set(true);
+//   m_in2.Set(false);
+//   m_pwm.SetSpeed(speed);
+
+// }
 
 void Robot::stopMotor(){
   m_in1.Set(false);
@@ -75,44 +77,61 @@ void Robot::stopMotor(){
 void Robot::AutonomousInit() {
   m_timer.Reset();
   m_timer.Start();
+  m_encoder.Reset();
+  m_pid.Reset();
+  //set our goal: move forward 1.0 meter
+  m_pid.SetSetpoint(1.0);
 }
 
-void Robot::moveBwd(double speed){
-  if(speed > 1.0){
-    speed = 1.0;
+// void Robot::moveBwd(double speed){
+//   if(speed > 1.0){
+//     speed = 1.0;
+//   }
+//   if(speed < 0.0){
+//     speed = 0.0;
+//   }
+//   m_in1.Set(false);
+//   m_in2.Set(true);
+//   m_pwm.SetSpeed(speed);
+// }
+
+void move(double speed){
+  speed = std::clamp(speed, -1.0, 1.0);
+  if(speed > 0){
+    m_in1.Set(true);
+    m_in2.Set(false);
   }
-  if(speed < 0.0){
-    speed = 0.0;
+  else if(speed < 0){
+    m_in1.Set(false);
+    m_in2.Set(true);
   }
-  m_in1.Set(false);
-  m_in2.Set(true);
-  m_pwm.SetSpeed(speed);
+  else{
+    m_in1.Set(false);
+    m_in2.Set(false);
+  }
+  m_pwm.SetSpeed(std::abs(speed));
 }
 
 void Robot::AutonomousPeriodic() {
-  units::time::second_t time = m_timer.Get();
 
-  if(time < 5_s){
-    moveFwd(1);
-  }
-  else if(time < 7_s){
+  //get data: read the current distance from encoder
+  double currentDistance = m_encoder.GetDistance();
+
+  //calculate: ask pid controller what speed we need to reach the setpoint 
+  //this returns a value between -1.0(full reverse) and 1.0 (full forward)
+  double outputSpeed = m_pid.Calculate(currentDistance);
+  outputSpeed = std::clamp(outputSpeed, -1.0, 1.0);
+
+  //actuate: send the data to motor functions
+
+  //check if we are at the distance(within tolerance)
+  if(m_pid.AtSetpoint()){
     stopMotor();
-  }
-  else if(time < 12_s){
-    moveBwd(1);
   }
   else{
-    stopMotor();
+    move(outputSpeed);
   }
-
 }
-
-//TODO: add use the PhysicsSim classes (like DifferentialDrivetrainSim or SwerveDriveSim), 
-//read the motor voltages, pass them to the simulator object, and then update your odometry with the new calculated position.
-void Robot::SimulationPeriodic(){
-
-}
-
 
 void Robot::TeleopInit() {}
 
--- RAS-RobotMain/src/main/include/Robot.h
+++ RAS-RobotMain/src/main/include/Robot.h
@@ -11,6 +11,8 @@
 #include <frc/PWM.h>
 #include <frc/DigitalOutput.h>
 #include <frc/Timer.h>
+#include <frc/Encoder.h> //library for encoders
+#include <frc/controller/PIDController.h> //library for PID
 
 class Robot : public frc::TimesliceRobot {
  public:
@@ -26,14 +28,11 @@ class Robot : public frc::TimesliceRobot {
   void TestPeriodic() override;
   void SimulationPeriodic() override;
 
-  void moveFwd(double speed);
-  void moveBwd(double speed);
+  void move(double speed);
   void stopMotor();
 
  private:
   frc::SendableChooser<std::string> m_chooser;
-  // const std::string kAutoNameDefault = "Default";
-  // const std::string kAutoNameCustom = "My Auto";
   std::string m_autoSelected;
   frc::Timer m_timer;
   frc::PWM m_pwm{0};
@@ -41,5 +40,6 @@ class Robot : public frc::TimesliceRobot {
   frc::DigitalOutput m_in2{1};
   frc::DigitalOutput m_in3{2};
   frc::DigitalOutput m_in4{3};
-
+  frc::Encoder m_encoder{4,5}; //Encoders use two Digital Input ports (channels). Let's assume 4 and 5.
+  frc::PIDController m_pid{1.0, 0.0, 0.0}; //P = 1.0 means if we are 1 meter away, go at speed 1.0 (full speed).
 };
