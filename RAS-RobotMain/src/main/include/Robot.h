a28e1499c3dc11853f648ec60c1b009b5b960997
luischihuahua <lachihuahua@miners.utep.edu>
2026-04-04 21:37:43 -0600

    Updated initial position and autonomous paths, first real test to see what happens with vision system

--- RAS-RobotMain/src/main/include/Robot.h
+++ RAS-RobotMain/src/main/include/Robot.h
@@ -140,7 +140,7 @@ class Robot : public frc::TimesliceRobot {
 
   //Pose initial point, first setpoint
   frc::Pose2d m_initialPose{0.78_m, 0.48_m, frc::Rotation2d{0_rad}}; //change values, wait for Justice
-  frc::Pose2d m_initialPose_BACKUP{0.79_m, 0.16_m, frc::Rotation2d{units::radian_t{-std::numbers::pi / 2.0}}};
+  frc::Pose2d m_initialPose_BACKUP{0.785_m, 0.13_m, frc::Rotation2d{units::radian_t{-std::numbers::pi / 2.0}}};
 
   //Estimator declaration
   frc::DifferentialDrivePoseEstimator field_poseEstimator{
